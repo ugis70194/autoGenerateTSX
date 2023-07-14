57ba2c6f5c84ee0a7f3e9837a724efc5bc1b3798
ugis70194 <ugis70194@yahoo.co.jp>
2023-07-14 20:20:10 +0900

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Toolkit } = require('actions-toolkit');
+const fs = require('fs');
+
+const targetDir = process.env.GITHUB_WORKSPACE+"/contents";
+
+Toolkit.run(async tools => {
+  console.log(targetDir);
+  try {
+    const works = fs.readdirSync(targetDir);
+  } catch (e) {
+    tools.log.fatal(e);
+    tools.exit.failure('Failed');
+  }
+})
