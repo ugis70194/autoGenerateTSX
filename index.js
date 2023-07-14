4db1302a15f1adebaa851f6db0fa85c5e0eb922d
ugis70194 <ugis70194@yahoo.co.jp>
2023-07-14 21:51:33 +0900

    add toTypeScriptFormat

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const { Toolkit } = require('actions-toolkit');
 const fs = require('fs');
 
-const targetDir = process.env.GITHUB_WORKSPACE+"/contents";
+const workingDir = process.env.GITHUB_WORKSPACE+"/contents";
+
+function toTypeScriptFromat(work, detail){
+  let body = "";
+  for(const key of Object.keys(detail)){
+    body += `${key}:"${detail[key]}",`;
+  }
+  body = body.slice(0,-1); // 末尾の , を削除
+  return `const ${work}: Work = {${body}}`
+}
 
 Toolkit.run(async tools => {
   console.log(targetDir);
   try {
-    const works = fs.readdirSync(targetDir);
-    console.log(works)
+    const works = fs.readdirSync(workingDir);
+    console.log(works);
+    for(const work of works){
+      const targetDir = workingDir + work; 
+      console.log(targetDir);
+      const detail = JSON.parse(fs.readFileSync(targetDir, "utf-8"));
+      console.log(detail);
+      console.log(toTypeScriptFromat(work, detail))
+    }
   } catch (e) {
     tools.log.fatal(e);
     tools.exit.failure('Failed');
