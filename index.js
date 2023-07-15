8e267880e1941ce56e5e144fd31fb8f9f93e20f3
ugis70194 <ugis70194@yahoo.co.jp>
2023-07-15 15:50:52 +0900

    書き込みテスト

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const jsonc = require('jsonc');
 const fs = require('fs');
 
 const appRoot = `${process.env.GITHUB_WORKSPACE}`;
-const workingDir = `${appRoot}/contents`;
-const genres = ["Manga", "Novel", "Illust", "CD", "Goods", "PhotoAlbum","Anthology", "Contribution", "Consideration"];
+const contentsDir = `${appRoot}/${process.env.INPUT_CONTENTS_DIRECTORY}`;
+const componentsDir = `${appRoot}/${process.env.INPUT_COMPONENTS_DIRECTORY}`;
 
 //detail.jsonに記載されたデータをTypeScriptのObjectに変換
 function toTypeScriptFromat(work, detail){
@@ -18,21 +18,33 @@ function toTypeScriptFromat(work, detail){
 
 //コンテンツとして存在しているジャンルの配列を返す
 function existGenre(works){
-  return [...new Set(works.map(work => work.genre))];
+  return [...new Set(works.map(work => {
+    const readTargetDir = `${contentsDir}/${work}`; 
+    const detailPath = `${readTargetDir}/${process.env.INPUT_TARGET_JSONC}`;
+    const detail = jsonc.parse(fs.readFileSync(detailPath, "utf-8"));
+    return detail.genre;
+  }))];
+}
+
+//空のコンポーネントの生成
+function generateEmptyComponent(genre){
+  const componentPath = `${componentsDir}/${genre}.tsx`;
+  fs.writeFileSync(componentPath, "");
+  console.log(`generated ${componentPath}`);
 }
 
 Toolkit.run(async tools => {
-  console.log(workingDir);
   try {
-    const works = fs.readdirSync(workingDir);
-    console.log(works);
+    const works = fs.readdirSync(contentsDir);
+    // 空のコンポーネントを生成
+    const genres = existGenre(works);
+    for(const genre of genres) generateEmptyComponent(genre);
+
     for(const work of works){
-      const readTargetDir = `${workingDir}/${work}`; 
-      const detailPath = `${readTargetDir}/detail.jsonc`;
-      console.log(readTargetDir);
+      const readTargetDir = `${contentsDir}/${work}`; 
+      const detailPath = `${readTargetDir}/${process.env.INPUT_TARGET_JSONC}`;
       const detail = jsonc.parse(fs.readFileSync(detailPath, "utf-8"));
-      console.log(detail);
-      console.log(toTypeScriptFromat(work, detail))
+      console.log(detail.genre);
     }
   } catch (e) {
     tools.log.fatal(e);
